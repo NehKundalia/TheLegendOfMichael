84bc1cacab50d12cb9f6d61f397531120f369f1a
Neh Kundalia <njk325@nyu.edu>
2019-05-15 06:42:13 -0400

    Final Project

---
+++ final_project/NYUCodebase/NYUCodebase/rpg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="rpg" tilewidth="16" tileheight="16" spacing="1" tilecount="486" columns="27">
+ <image source="../Tilemap/tilemap.png" trans="9ae5ed" width="458" height="305"/>
+</tileset>
