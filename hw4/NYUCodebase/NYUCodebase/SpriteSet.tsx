592d17d1ac7a59864ae193e2400bb45fe67535f6
Neh Kundalia <36010959+NehKundalia@users.noreply.github.com>
2021-08-25 05:32:52 -0400

    Delete hw4/NYUCodebase directory

--- hw4/NYUCodebase/NYUCodebase/SpriteSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="SpriteSet" tilewidth="23" tileheight="23" tilecount="900" columns="30">
- <image source="spritesheet_rgba.png" trans="9ae5ed" width="692" height="692"/>
-</tileset>
